bdd2883c5b3d2ae2ac484bcc4264817360c074e9
Yauri Attamimi <yauritux@gmail.com>
2024-09-18 06:33:53 +0700

    test: implement test cases for adding an employee

---
+++ src/test/java/com/cercli/application/EmployeeServiceTest.java
@@ -0,0 +1,67 @@
+package com.cercli.application;
+
+import com.cercli.domain.Employee;
+import com.cercli.domain.EmployeeRepository;
+import com.cercli.infrastructure.InMemoryEmployeeRepository;
+import com.cercli.shared.exception.EmployeeCreationFailedException;
+import com.cercli.shared.exception.EmployeeNotFoundException;
+import com.cercli.shared.util.DateTimeUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class EmployeeServiceTest {
+
+    private EmployeeRepository employeeRepository;
+    private EmployeeService employeeService;
+
+    @BeforeEach
+    public void setUp() {
+        employeeRepository = new InMemoryEmployeeRepository();
+        employeeService = new EmployeeService(employeeRepository);
+    }
+
+    @Test
+    public void testAddEmployee() {
+        Employee employee = new Employee(
+                UUID.randomUUID(),
+                "Yauri Attamimi",
+                "AI Engineer",
+                "yauritux@gmail.com",
+                11000,
+                DateTimeUtils.getCurrentDateTimeInServerTimeZone(),
+                DateTimeUtils.getCurrentDateTimeInServerTimeZone()
+        );
+
+        employeeService.addEmployee(employee);
+        Employee retrievedEmployee = employeeService.getEmployee(employee.id());
+
+        assertNotNull(retrievedEmployee);
+        assertEquals(employee.id(), retrievedEmployee.id());
+    }
+
+    @Test
+    public void testAddEmployeeWithInvalidEmail() {
+        Employee employee = new Employee(
+                UUID.randomUUID(),
+                "Yauri Attamimi",
+                "AI Engineer",
+                "yauritux",
+                9000,
+                DateTimeUtils.getCurrentDateTimeInServerTimeZone(),
+                DateTimeUtils.getCurrentDateTimeInServerTimeZone()
+        );
+
+        var thrown = assertThrows(EmployeeCreationFailedException.class, () -> {
+            employeeService.addEmployee(employee);
+        });
+
+        assertEquals(String.format(
+                "Invalid email for employee: %s", employee.email()), thrown.getMessage());
+    }
+}
