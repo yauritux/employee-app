1f3dce2bf22d44f6f486848cd812a7f68e1a6bc1
Yauri Attamimi <yauritux@gmail.com>
2024-09-18 08:06:00 +0700

    feat: introduce new exception for TimeOffRequest violation

---
+++ src/main/java/com/cercli/shared/exception/TimeOffRequestException.java
@@ -0,0 +1,12 @@
+package com.cercli.shared.exception;
+
+/**
+ * @author Yauri Attamimi (yauritux@gmail.com)
+ * @version 1.0
+ */
+public class TimeOffRequestException extends RuntimeException {
+
+    public TimeOffRequestException(String message) {
+        super(message);
+    }
+}
