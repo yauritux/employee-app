fff9d302617b64ff7a39a9af9933639404cb3cc5
Yauri Attamimi <yauritux@gmail.com>
2024-09-18 07:21:56 +0700

    feat: implement request category

---
+++ src/main/java/com/cercli/domain/RequestCategory.java
@@ -0,0 +1,11 @@
+package com.cercli.domain;
+
+import java.util.UUID;
+
+/**
+ * @author Yauri Attamimi (yauritux@gmail.com)
+ * @version 1.0
+ *
+ * Represents a request category.
+ */
+public record RequestCategory(UUID id, String name) {}
---
+++ src/main/java/com/cercli/domain/RequestCategoryRepository.java
@@ -0,0 +1,22 @@
+package com.cercli.domain;
+
+import java.util.UUID;
+
+/**
+ * @author Yauri Attamimi (yauritux@gmail.com)
+ * @version 1.0
+ *
+ * Interface representing a repository for managing RequestCategory entities.
+ * This interface represents `what so-called as `port` within the context of
+ * <strong>Hexagonal Architecture</strong>.
+ */
+public interface RequestCategoryRepository {
+
+    /**
+     * Retrieves a request category by its ID.
+     *
+     * @param id the ID of the request category.
+     * @return the request category with the specified ID, or null if not found.
+     */
+    RequestCategory getRequestCategoryById(UUID id);
+}
